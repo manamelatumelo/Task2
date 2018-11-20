df9185f4e05b86da162ff80dc0d02ee3bc639d44
tumelo <tumelo@mamoletu.co.za>
2018-11-20 07:44:34 +0200

    Added Nunit class for test cases

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Task2
 Create test cases for web link 
+
+Task 2 -Web 
+*****
+Date : 19 November 2018
+Author : Manamela Tumelo
+Email : mtlmanamela@gmail.com
+
+******
+
+*I have been asked to create test case based on this link http://www.way2automation.com/angularjs-protractor/webtables/ 
+tasks that are to be perform are as follows.
+
+	 1. Validate that I am on the list of users on the above link
+	 2. Click Add User
+	 3. Add users with the details provided on the assesment documents.
+
+* Tools/Techonlogy used
+	1. For this test case I created a  console application (C#) .net 4.7.2 
+	2. installed Selenium webdriver package for solution 
+	3. installed chrome drive from the Packages for solution
+	4. installed NUnitFramework
+	5. installed nunitTestAdapter.WithFramework
+
+
+	
+
+
+	
+
---
+++ Task 2 - Web/Nunit.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace Task_2___Web
+{
+    class NUnit
+    {
+        IWebDriver driver = new ChromeDriver();
+        [SetUp]
+
+        public void InitializeBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void TestNUnit()
+        {
+            driver.Navigate().GoToUrl("http://www.way2automation.com/angularjs-protractor/webtables/");
+        }
+        //[Test]
+        //public void ValidateUser()
+        //{
+        //    driver.FindElement(By.)
+        //}
+        [TearDown]
+        public void CloseBrower()
+        {
+            driver.Close();
+        }
+    }
+}
